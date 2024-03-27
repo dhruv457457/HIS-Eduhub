7bc8d1a5179716f036724417c5ba84f198101215
Shaurya01836 <shaurya01836@gmail.com>
2024-03-27 16:50:27 +0530

    carousel

--- js/script.js
+++ js/script.js
@@ -44,3 +44,18 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+// for carousel
+let slideIndex = 0;
+
+        function showSlides() {
+            const slides = document.querySelectorAll('.carousel-slide img');
+            slides.forEach(slide => {
+                slide.style.display = 'none';
+            });
+            slideIndex++;
+            if (slideIndex > slides.length) { slideIndex = 1 }
+            slides[slideIndex - 1].style.display = 'block';
+            setTimeout(showSlides, 3000); // Change slide every 3 seconds
+        }
+
+        showSlides();
